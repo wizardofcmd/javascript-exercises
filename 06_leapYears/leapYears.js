1ef33974d8f3ac1d9c40f8b97ae3b3a66f69b769
Sorin <sorinnechiforora@gmail.com>
2022-08-28 20:25:19 +0100

    Fix leapYears tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    centuryYear = ((year % 100) == 0) && ((year % 400) == 0);
+    notCenturyYear = ((year % 100) == 0) && ((year % 400) != 0)
 
+    if (centuryYear && (year % 4) == 0) {
+        return true;
+    }
+    else if (notCenturyYear) {
+        return false;
+    }
+    else if ((year % 4) == 0) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
