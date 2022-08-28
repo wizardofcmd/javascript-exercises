ae37cf6b292dbec3a88de5e2b0c4ad5af3bffde8
Sorin <sorinnechiforora@gmail.com>
2022-08-28 14:32:08 +0100

    Fix repeatString tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let newString = '';
 
-};
+    if (num < 0 || isNaN(num)) {
+        return "ERROR";
+    }
+
+    for (let i = 0; i < num; i++) {
+        newString += str;
+    }
+    return newString;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
