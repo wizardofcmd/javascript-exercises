c27528febb50d90ed5d83365bed1080042cdb01b
Sorin <sorinnechiforora@gmail.com>
2022-08-28 19:21:20 +0100

    Fix removeFromArray tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...removal) {
+    let newArr = [];
+    let itemsToRemove = [];
+    removal.forEach(arg => itemsToRemove.push(arg));
+    console.log(removal);
 
+    for (const item of arr) {
+        newArr.push(item);
+        if (itemsToRemove.includes(item)) {
+            removeItemIndex = itemsToRemove.indexOf(item);
+            removeItemType = typeof(itemsToRemove[removeItemIndex]);
+
+            itemType = typeof(item);
+            
+            if (itemType === removeItemType) {
+                newArr.pop();
+            }
+        }
+    }
+    return newArr;
 };
 
 // Do not edit below this line
