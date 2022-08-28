c38b645423a7f5d0a1fd9f16c2571d2502d959a8
Sorin <sorinnechiforora@gmail.com>
2022-08-28 19:53:10 +0100

    Fix sumAll tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    let start = 0;
+    let end = 0;
+    let sum = 0;
 
+    if ((numOne < 0 || numTwo < 0) || ((isNaN(numOne) || isNaN(numTwo)) )
+    || (typeof(numOne) != "number") || typeof(numTwo) != "number"){
+        return "ERROR";
+    }
+    else {
+        if (numTwo > numOne) {
+            start = numOne;
+            end = numTwo;
+        }
+        else {
+            start = numTwo;
+            end = numOne;
+        }
+        
+        for (let i = start; i <= end; i++) {
+            sum += i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
