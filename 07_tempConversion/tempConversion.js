a07cc46e4cc7e6894e8519ec8e3e36d9969b42b2
Sorin <sorinnechiforora@gmail.com>
2022-08-28 21:12:49 +0100

    Fix tempConversion tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  conversion = (fahrenheit - 32) * (5/9);
+  conversion = Math.round(conversion * 10) / 10;
+  return conversion;
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  conversion = celsius * (9/5) + 32;
+  conversion = Math.round(conversion * 10) / 10;
+  return conversion;
 };
 
 // Do not edit below this line
