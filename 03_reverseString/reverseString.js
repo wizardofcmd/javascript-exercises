48bb45324fad1bce276b2171f839cb337398e24a
Sorin <sorinnechiforora@gmail.com>
2022-08-28 17:54:29 +0100

    Fix reverseString tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let reversedStr = '';
 
+    for (let i = 1; i < str.length + 1; i++){
+        reversedStr += str[str.length - i];
+    }
+    return reversedStr;
 };
 
 // Do not edit below this line
