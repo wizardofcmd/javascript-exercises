27fec024b9e41bb4e2ad3e6ae7243e58cfab2e35
Sorin <sorinnechiforora@gmail.com>
2023-01-14 14:53:41 +0000

    Add missing functionality to palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+    filteredPhrase = phrase.replace(/[^\w ]/g, '').toLowerCase();
+    filteredPhrase = filteredPhrase.replace(/\s/g, "");
+    let phraseBackwards = "";
+    for (let i = filteredPhrase.length - 1; i >= 0; i--) {
+        phraseBackwards += filteredPhrase[i];
+    }
+    console.log(`${phraseBackwards}\n${filteredPhrase}`);
+    return phraseBackwards === filteredPhrase ? true : false;
 };
 
 // Do not edit below this line
