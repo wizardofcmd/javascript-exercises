ea1bc004071fef5c2f1fa21ce1405d3da2d2134a
Sorin <sorinnechiforora@gmail.com>
2023-01-14 18:24:09 +0000

    Add missing functionality to findTheOldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    let age = 0;
+    let oldest = 0;
+    let oldestPerson = [];
+    people.forEach(calcAge);
+    return oldestPerson;
 
+    function calcAge(person) {
+        if (!("yearOfDeath" in person)) {
+            yearTo = new Date().getFullYear()
+        }
+        else {
+            yearTo = person.yearOfDeath;
+        }
+
+        age = yearTo - person.yearOfBirth;
+        if (oldest < age) {
+            oldest = age;
+            oldestPerson = person;
+        }
+    };
 };
 
 // Do not edit below this line
